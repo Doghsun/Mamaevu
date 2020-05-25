f15bbef4f03f7a3e4588a3746d1e013e5e28edb8
dev <dev@mail.com>
2020-05-26 07:32:40 +0900

    Updates

---
+++ precache-manifest.d39447b27becc65f0461b16a27c23dd8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f9679d22cdf36f7245611def47ca2a",
+    "url": "/Mamaevu/index.html"
+  },
+  {
+    "revision": "3c13271e31c59ab6f118",
+    "url": "/Mamaevu/static/css/main.470a7d4c.chunk.css"
+  },
+  {
+    "revision": "343b3be27c6b188456b2",
+    "url": "/Mamaevu/static/js/2.03b2b789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mamaevu/static/js/2.03b2b789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c13271e31c59ab6f118",
+    "url": "/Mamaevu/static/js/main.bce0b38f.chunk.js"
+  },
+  {
+    "revision": "136840a532bb16ee8e53",
+    "url": "/Mamaevu/static/js/runtime-main.369cb96c.js"
+  },
+  {
+    "revision": "f6449a2a7a52d1a4b292260152ea6b8a",
+    "url": "/Mamaevu/static/media/Cutar.f6449a2a.png"
+  },
+  {
+    "revision": "887f51647efe83210359c3b5eefa67b9",
+    "url": "/Mamaevu/static/media/Efim.887f5164.png"
+  },
+  {
+    "revision": "4b0036b8e8aa1e1f17a12242bf1d4ad1",
+    "url": "/Mamaevu/static/media/Filipp.4b0036b8.png"
+  },
+  {
+    "revision": "8c0da4f2613766eb5eb3e04057ff2036",
+    "url": "/Mamaevu/static/media/Innokentiy.8c0da4f2.png"
+  },
+  {
+    "revision": "f592da205e0c5cb461b447c671ddd2a0",
+    "url": "/Mamaevu/static/media/Kirill.f592da20.png"
+  },
+  {
+    "revision": "01113cee91ae5923158e83971756559e",
+    "url": "/Mamaevu/static/media/Mamaevu.01113cee.png"
+  },
+  {
+    "revision": "2bca882bf8a54be9ba2f8bb8335d89a7",
+    "url": "/Mamaevu/static/media/N_MarkovI.2bca882b.png"
+  },
+  {
+    "revision": "125559ba130beaf321db058d89db99e8",
+    "url": "/Mamaevu/static/media/Nikolay_F_M.125559ba.png"
+  }
+]);
